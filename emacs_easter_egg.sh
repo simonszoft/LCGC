389cf991513956a8e7ea2358f041d96575764eb8
Simon Nándor <simonszoft@gmail.com>
2019-10-01 00:48:23 +0200

    Update emacs_easter_egg.sh

--- emacs_easter_egg.sh
+++ emacs_easter_egg.sh
@@ -1,1 +1,14 @@
-emacs -batch -l dunnet
+#!/bin/bash
+
+# Simonszoft - Linux Console Easter Egg - Emacs
+# https://github.com/simonszoft/LCGC
+	
+if ! [ -x "$(command -v emacs)" ]; then
+    echo 'Error: emacs is not installed.' >&2	
+    exit 1
+else
+    emacs -batch -l dunnet
+fi
+	
+
+
