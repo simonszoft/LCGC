a727e01c59efca23495eec9cd30f3e761154bce9
Simon Nándor <simonszoft@gmail.com>
2019-10-01 00:56:46 +0200

    Update apt_easter_egg.sh

--- apt_easter_egg.sh
+++ apt_easter_egg.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
+
+# Simonszoft - Linux Console Easter Egg - Emacs
+# https://github.com/simonszoft/LCGC
+
+# I.
+echo "MOO"
 apt-get moo
+
+# II.
+echo "1."
+echo 
+aptitude -v moo
+
+echo "2."
+echo 
+aptitude -vv moo
+
+echo "3."
+echo 
+aptitude -vvv moo
+
+echo "4."
+echo 
+aptitude -vvvv moo
+
+echo "5."
+echo 
+aptitude -vvvvv moo
+
+echo "6."
+echo 
+aptitude -vvvvvv moo
+
+echo "7."
+echo 
+aptitude -vvvvvvv moo
