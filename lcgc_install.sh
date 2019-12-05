b0b7cf6d095901f8a8138b404c9f17e468235197
Simon Nándor <simonszoft@gmail.com>
2019-12-05 22:53:10 +0100

    Update lcgc_install.sh

--- lcgc_install.sh
+++ lcgc_install.sh
@@ -7,11 +7,13 @@ if [[ $EUID > 0 ]]; then
 	echo "Please run as root/sudo"
 	exit 1
 else
-	echo "APT source list update"
-	apt-get update
-	echo "Installing games"
-	apt-get install asciijump bastet bsdgames crawl greed moon-buggy nettoe ninvaders nsnake pacman4console sl sudoku zangband
+	echo ".: Linux Console Games collection :."
 	
+	echo "1. APT source list update"
+	apt-get update
+	
+	echo "2. Installing games"
+	apt-get install asciijump bastet bsdgames crawl greed moon-buggy nettoe ninvaders nsnake pacman4console sl sudoku zangband
 	
 	if ! [ -x "$(command -v wget)" ]; then
   		echo 'Error: wget is not installed.' >&2	
@@ -20,5 +22,6 @@ else
 		wget "https://raw.githubusercontent.com/simonszoft/LCGC/master/README.md" -O - -q 
 	fi
 	
+	echo "3. Done"
 fi
 
